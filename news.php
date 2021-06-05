245d1fbf0dbec5bb764b4aefd23d475eb5f821d2
ManhHung-2k2 <78028211+ManhHung-2k2@users.noreply.github.com>
2021-06-05 16:43:58 +0700

    Add files via upload

---
+++ news.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tin tức</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+				<h1 style="padding: 10px; color:red;">TIN TỨC THỂ THAO</h1>
+				<div class="row">
+				<div class="col-md-6">		
+				<img style="width:540px;height: 288px;" src="https://viypa.vn/wp-content/uploads/2018/11/yoga.3jpg-800x480.jpg">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Yoga không chỉ là môn thể thao </a></h4>
+					</div>
+
+				<div class="col-md-6">
+						<img style="width:540px;height: 288px" src="https://hinh365.com/wp-content/uploads/2020/06/b1d062d168f56af31267b89d0d62df6f.jpg">
+						<h4 style="font-size: 20px; padding: 10px;" ><a href="#">Boxing những điều cần lưu ý</a></h4>
+				</div>
+				
+					<div class="col-md-6">
+							<img style="width:540px;height: 288px" src="https://cdnimg.vietnamplus.vn/uploaded/bokttj/2015_08_15/ttxvn_anh_vien.jpg">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Bơi lội môn thể thao dưới nước</a></h4>
+					</div>
+					<div class="col-md-6">
+							<img style="width:540px;height: 288px" src="https://c.wallhere.com/photos/03/ac/tennis_Roger_Federer-237317.jpg!d">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Tennis sân chơi bổ ích</a></h4>
+					</div>
+					<div class="col-md-6">
+							<img style="width:540px;height: 288px" src="https://media.vneconomy.vn/w800/images/upload/2021/04/20/bong-chuyen-1519971336531989028933-113-0-688-1024-crop-1519971342424698746351.jpg">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Bóng chuyền môn thể thao đồng đội</a></h4>
+					</div>
+					<div class="col-md-6">
+							<img style="width:540px;height: 288px" src="https://file.hstatic.net/1000257596/article/1qbuom-e1502826154360-1-1068x668_1024x1024.jpg">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Bóng rổ nơi cảm xúc vỡ òa</a></h4>
+					</div>
+					<div class="col-md-6">
+							<img style="width:540px;height: 288px" src="https://vi.sblaw.vn/wp-content/uploads/2017/11/Khi-th%E1%BB%B1c-hi%E1%BB%87n-th%C3%B4ng-b%C3%A1o-khuy%E1%BA%BFn-m%E1%BA%A1i-c%C3%B3-c%E1%BA%A7n-n%C3%AAu-%C4%91%E1%BB%8Ba-%C4%91i%E1%BB%83m-c%E1%BB%A5-th%E1%BB%83-sblaw.jpg">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Sale cực sốc từ ngày 10/6</a></h4>
+					</div>
+					<div class="col-md-6">
+							<img style="width:540px;height: 288px" src="https://media.vneconomy.vn/w800/images/upload/2021/06/01/sacombank-khai-he-ruc-ro-mo-man-trieu-deal.jpg">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Tri ân khách hàng</a></h4>
+					</div>
+
+			</div>
+	</div>
+</body>
+</html>
