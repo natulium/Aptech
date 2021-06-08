489acae9ec65d696bfacffb925a6e5805ebf4d70
ManhHung-2k2 <78028211+ManhHung-2k2@users.noreply.github.com>
2021-06-08 15:22:57 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Footer</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+	<div class="footer">
+    		<div class="row">
+    			<div class="col-md-3">
+    				<b>Công ty</b>
+    				<ul>
+    				<li>Tuyển dụng</li>
+    				<li>Tin tức</li>
+    				<li>Quy định bảo mật</li>
+    				<li>Điều khoản & điều kiện</li>
+    			</ul>
+    			</div>
+    			<div class="col-md-3">
+    				<b>Cộng đồng</b>
+    				<ul>
+    					<li>Blog</li>
+    					<li>Facebook</li>
+    					<li>Instagram</li>
+    					<li>Youtube</li>
+    				</ul>
+    			</div>
+    			<div class="col-md-3">
+    				<b>Chương trình tập</b>
+    				<ul>
+    					<li>HLV cá nhân</li>
+    					<li>Lớp tập nhóm</li>
+    					<li>Gói tập doanh nghiệp</li>
+    				</ul>
+    			</div>
+    			<div class="col-md-3">
+    				<b>Trang chủ</b>
+    				<ul>
+    					<li>Tìm CLB gần bạn</li>
+    					<li>Gia nhập Elite Fitness</li>
+    					<li>Đăng ký gói tập miễn phí</li>
+    					<li>Các gói tập</li>
+    					<li>Đăng nhập</li>
+    				</ul>
+    				
+    			</div>
+    		</div>
+    </div>
+    <div class="col-6">
+    	<b>Hotline: 19009091</b>
+    	<br>
+    	<b>Email: info@elitefiness.com.vn</b>
+    </div>
+    <div class="col-4">
+    	<img src="https://img3.thuthuatphanmem.vn/uploads/2019/09/30/anh-logo-hinh-canh-hoa_111808231.jpg">
+    </div>
+</body>
+</html>
