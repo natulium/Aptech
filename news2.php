44f9199ab73df0be1ab68f3b2a687ccc3625149a
ManhHung-2k2 <78028211+ManhHung-2k2@users.noreply.github.com>
2021-06-09 14:24:21 +0700

    Add files via upload

---
+++ news2.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style2.css">
+	<title>New2s </title>
+</head>
+<body>
+		<div id="printcontent">
+			<div class="container">
+				<div class="row">
+					<div class="col-12" style="text-align: center;">
+						<p style="margin: 20px 0 20px">
+							<font style="vertical-align: inherit;">09/06/2021 bởi Elite</font>
+						</p>
+						<h1>
+							<font style="vertical-align: inherit;"> [THÔNG BÁO] Giờ mở cửa trong mùa hè này </font>
+						</h1>
+					</div>	
+				</div>				
+			</div>		
+		</div>
+
+	<div class="container-fluid" style="background: #f0f0f0;position: relative;padding-top: 30px">
+		<div style="position: absolute;background: #fff;width: 100%;height: 30px;bottom: 0;">
+			<div class="container">
+				<div class="row">
+					<div class="col-12" style="margin-top:30px;">
+			<img src="https://cms.elitefitness.com.vn//wp-content/uploads/notice-banner.png" style="width: 100%;">   </div>		
+				</div>		
+			</div>
+			
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+		<div class="col-12">
+			<div class="post_content" style="margin: 40px 150px 0 150px;padding-top:320px;">
+				<p>
+				<span style="font-family:arial;font-size: 23px;">
+					<font style="vertical-align: inherit;">Elite vui mừng thông báo giờ mở cửa của các Câu lạc bộ Elite trong dịp lễ. Vui lòng xem chi tiết bên dưới </font>
+						<br>
+				<font style="vertical-align: inherit; ">Hội viên của Elite cũng có thể xem lịch học cập nhật qua:<a href="https://baomoi.com">https://baomoi.com</a> </font>
+				<br>
+				<font style="vertical-align: inherit;">Chúc các bạn có mùa hè đầy năng lượng.</font>
+				</span>
+				</p>
+			</div>
+		</div>
+		</div>
+	</div>
+	<div class="container section" style="margin-top: 30px;">
+		<div class="row">
+			<div class="col-12">
+			<h1>
+				<font style="vertical-align: inherit; color: #e2e2e2 ">TIN LIÊN QUAN</font>
+			</h1>	
+			</div>
+			
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-12 col-md-6">
+			<div class="blog">
+				<a href="#">
+				<div>
+					<img style="width: 100%;" src="https://cms.elitefitness.com.vn//wp-content/uploads/2019/Thang_8/Nghi_le_2_9/ava.jpg?w=517&h=310&mode=stretch">
+				</div>
+				<p>
+					<font style="vertical-align: inherit; color: black;padding-left: 20px;font-size:23px;">Khuyến mãi cực xốc khi mùa hè đến cho các thành viên trong câu lạc bộ</font>
+				</p>
+			</a>
+			</div>
+		</div>
+		<div class="col-12 col-md-6">
+			<div class="blog">
+				<a href="#">
+				<div>
+					<img style="width: 100%;" src="https://cms.elitefitness.com.vn//wp-content/uploads/2019/Thang_8/Nghi_le_2_9/ava.jpg?w=517&h=310&mode=stretch">
+				</div>
+				<p>
+					<font style="vertical-align: inherit; color: black;padding-left: 20px;font-size:23px;">Mùa hè đến với nhiều hoạt động thú vị cùng các câu lạc bộ thể thao</font>
+				</p>
+			</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
