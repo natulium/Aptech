e09a90aca7179271532670b172620ebde3ea4449
ManhHung-2k2 <78028211+ManhHung-2k2@users.noreply.github.com>
2021-06-09 15:44:13 +0700

    Add files via upload

--- news.php
+++ news.php
@@ -13,41 +13,41 @@
 </head>
 <body>
 	<div class="container">
-				<h1 style="padding: 10px; color:red;">TIN TỨC THỂ THAO</h1>
+				<h1 style="padding: 40px; color:red;">TIN TỨC THỂ THAO</h1>
 				<div class="row">
 				<div class="col-md-6">		
-				<img style="width:540px;height: 288px;" src="https://viypa.vn/wp-content/uploads/2018/11/yoga.3jpg-800x480.jpg">
-						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Yoga không chỉ là môn thể thao </a></h4>
+				<img style="width:540px;height: 288px;" src="https://cms.elitefitness.com.vn//wp-content/uploads/2018/News/Dong cua Xuan Dieu/ava.jpg?w=517&h=310&mode=stretch">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Thông báo chính thức: Elite Fitness Hà Nội tạm thời đóng cửa để tu sửa từ ngày 16/02/2021 đến 21/02/2021</a></h4>
 					</div>
 
 				<div class="col-md-6">
-						<img style="width:540px;height: 288px" src="https://hinh365.com/wp-content/uploads/2020/06/b1d062d168f56af31267b89d0d62df6f.jpg">
-						<h4 style="font-size: 20px; padding: 10px;" ><a href="#">Boxing những điều cần lưu ý</a></h4>
+						<img style="width:540px;height: 288px" src="https://cms.elitefitness.com.vn/wp-content/uploads/detail_club/Vinh.jpg">
+						<h4 style="font-size: 20px; padding: 10px;" ><a href="#">Thông báo: Elite Fitness Hà Nội đã trở lại sau những ngày tu sửa</a></h4>
 				</div>
 				
 					<div class="col-md-6">
-							<img style="width:540px;height: 288px" src="https://cdnimg.vietnamplus.vn/uploaded/bokttj/2015_08_15/ttxvn_anh_vien.jpg">
-						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Bơi lội môn thể thao dưới nước</a></h4>
+							<img style="width:540px;height: 288px" src="https://cf.shopee.vn/file/decb8bec4f5867c424408028c9461f76">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Tri ân Hội viên: Tặng 20000 Voucher cho học viên tại Elite Fitness </a></h4>
 					</div>
 					<div class="col-md-6">
-							<img style="width:540px;height: 288px" src="https://c.wallhere.com/photos/03/ac/tennis_Roger_Federer-237317.jpg!d">
-						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Tennis sân chơi bổ ích</a></h4>
+							<img style="width:540px;height: 288px" src="https://img.kam.vn/images/375x0/91e8b7c58aaf42fc8e5976097c41d11e/elite-fitness-tang-toi-19-trieu-cho-the-tap-12-thang.jpg">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Ưu đãi đặc biệt: Khuyến mãi cho học viên đăng kí gói tập 12 tháng</a></h4>
 					</div>
 					<div class="col-md-6">
-							<img style="width:540px;height: 288px" src="https://media.vneconomy.vn/w800/images/upload/2021/04/20/bong-chuyen-1519971336531989028933-113-0-688-1024-crop-1519971342424698746351.jpg">
-						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Bóng chuyền môn thể thao đồng đội</a></h4>
+							<img style="width:540px;height: 288px" src="https://cms.elitefitness.com.vn//wp-content/uploads/2018/B%C3%A0i%20web/Th%C3%A1ng%2010/10%20minute%20active/banner_baiviet.jpg">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">HOT: Tham gia cuộc thi để nhận ngay giải thưởng lớn với Elite Fitness</a></h4>
 					</div>
 					<div class="col-md-6">
-							<img style="width:540px;height: 288px" src="https://file.hstatic.net/1000257596/article/1qbuom-e1502826154360-1-1068x668_1024x1024.jpg">
-						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Bóng rổ nơi cảm xúc vỡ òa</a></h4>
+							<img style="width:540px;height: 288px" src="https://cms.elitefitness.com.vn//wp-content/uploads/avatar%20-%20pt%20online.jpg?w=517&h=310&mode=stretch">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">HOT: Luyện tập chống dịch tại nhà cùng với Elite Fitness</a></h4>
 					</div>
 					<div class="col-md-6">
-							<img style="width:540px;height: 288px" src="https://vi.sblaw.vn/wp-content/uploads/2017/11/Khi-th%E1%BB%B1c-hi%E1%BB%87n-th%C3%B4ng-b%C3%A1o-khuy%E1%BA%BFn-m%E1%BA%A1i-c%C3%B3-c%E1%BA%A7n-n%C3%AAu-%C4%91%E1%BB%8Ba-%C4%91i%E1%BB%83m-c%E1%BB%A5-th%E1%BB%83-sblaw.jpg">
-						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Sale cực sốc từ ngày 10/6</a></h4>
+							<img style="width:540px;height: 288px" src="https://cms.elitefitness.com.vn/wp-content/uploads/2020/Dong%20cua%20TD%20v%C3%A0%20DK/closing%20notice%20avt-08.png">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Thông Báo Khẩn: Dịch bệnh phức tạp Elite Finess tạm thời đóng cửa từ ngày 02/05/2021</a></h4>
 					</div>
 					<div class="col-md-6">
-							<img style="width:540px;height: 288px" src="https://media.vneconomy.vn/w800/images/upload/2021/06/01/sacombank-khai-he-ruc-ro-mo-man-trieu-deal.jpg">
-						<h4 style="font-size: 20px; padding: 10px;"><a href="#">Tri ân khách hàng</a></h4>
+							<img style="width:540px;height: 288px" src="https://cms.elitefitness.com.vn/wp-content/uploads/notice-avt.png">
+						<h4 style="font-size: 20px; padding: 10px;"><a href="#">HOT: Elite Finess sẽ trở lại từ ngày 15/05/2021 chào đón mùa hè đến</a></h4>
 					</div>
 
 			</div>
