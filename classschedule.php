2c4dc170b9c0bc2fc5f39f88a1885da87e38fd54
ManhHung-2k2 <78028211+ManhHung-2k2@users.noreply.github.com>
2021-06-16 22:48:44 +0700

    Add files via upload

---
+++ classschedule.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Schedule</title>
+	<meta charset="utf-8">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="styleschedule.css">
+</head>
+<body>
+	<img src="https://i1.wp.com/www.jccatlantic.org/wp-content/uploads/2020/09/Group-Exercise-Schedule-Page-Image2.jpg?fit=1200%2C630&ssl=1" style="width: 100%;height: 530px;object-fit: cover; margin-top: 10px;margin-bottom: 20px">
+	<div>
+		<div class="container-fluid " style="border-bottom: 2px">
+			<div class="container" style="position: relative">
+				<div class="row" style="text-align: center;font-weight:500;font-size: 50px;position: relative;">
+					<div class="col">
+						<b>Mon</b>
+						<h6>14/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Tue</b>
+						<h6>15/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Wed</b>
+						<h6>16/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Thu</b>
+						<h6>17/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Fri</b>
+						<h6>18/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Sat</b>
+						<h6>19/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Sun</b>
+						<h6>20/06/2021</h6>
+					</div>
+
+		<img src="https://img.favpng.com/6/23/17/computer-icons-png-favpng-Jj3idVigvuN67Lb48pw23xMZd.jpg" style="width: 45px;position: absolute;top:30px;left:-20px;cursor: pointer;">
+					<img src="https://e7.pngegg.com/pngimages/390/296/png-clipart-arrow-computer-icons-right-angle-symbol-arrow-left-angle-logo.png"style="width:45px;position: absolute;top:30px;right:-20px;cursor: pointer;">
+				</div>
+			</div>
+		</div>
+	</div>
+					<div class="schedule-title" style="padding: 0px;">
+						<p>Morning</p>	
+					</div>	
+			
+				<div class="schedule-row">
+					<div class="col" id="monday">
+						<div class="schedule">
+							<p>Boxing</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>Boxing</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>Boxing</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+					</div>
+					<div class="col" id="tueday"></div>
+					<div class="col" id="wednesday"></div>
+					<div class="col" id="thursday"></div>
+					<div class="col" id="friday">
+						<div class="schedule">
+							<p>TEN SO 1</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 2</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 3</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+					</div>
+					<div class="col" id="Saturday"> 
+						<div class="schedule">
+							<p>TEN SO 1</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 2</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 3</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+					</div>
+					<div class="col" id="Sunday"></div>
+				</div>
+				<div class="schedule-title" style="padding: 0px;">
+					<p>Afternoon</p>
+				</div>			
+
+				<div class="schedule-row">
+					<div class="col" id="monday1"></div>
+					<div class="col" id="tueday1">
+						<div class="schedule">
+							<p>TEN SO 1</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 2</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 3</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+					</div>
+					<div class="col" id="wednesday1">
+							<div class="schedule">
+							<p>TEN SO 1</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 2</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 3</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+					</div>
+					<div class="col" id="thursday1">
+						<div class="schedule">
+							<p>TEN SO 1</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 2</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 3</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+					</div>
+					<div class="col" id="friday1"></div>
+					<div class="col" id="saturday1"></div>
+					<div class="col" id="sunday1">
+						<div class="schedule">
+							<p>TEN SO 1</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 2</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+						<div class="schedule">
+							<p>TEN SO 3</p>
+							<p>06:30-07:30</p>
+							<p>Boxing </p>
+						</div>
+					</div>
+
+
+				</div>
+
+			
+			
+
+			
+
+</body>
+</html>
