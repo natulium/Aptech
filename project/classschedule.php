120a6c0ca5586de9c3681c4817e74c04983b1346
ManhHung-2k2 <78028211+ManhHung-2k2@users.noreply.github.com>
2021-06-17 16:51:20 +0700

    Add files via upload

---
+++ project/classschedule.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <meta charset='utf-8'>
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="styleschedule.css">
+</head>
+<body>
+
+	<img src="https://i1.wp.com/www.jccatlantic.org/wp-content/uploads/2020/09/Group-Exercise-Schedule-Page-Image2.jpg?fit=1200%2C630&ssl=1" style="width: 100%;height: 530px;object-fit: cover; margin-top: 10px;margin-bottom: 20px">
+	<div>
+		<div class="container-fluid " style="border-bottom: 2px">
+			<div class="container" style="position: relative">
+			<div class="row" style="text-align: center;font-weight:500;font-size: 50px;position: relative;">
+					<div class="col">
+						<b>Mon</b>
+						<h6>14/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Tue</b>
+						<h6>15/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Wed</b>
+						<h6>16/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Thu</b>
+						<h6>17/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Fri</b>
+						<h6>18/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Sat</b>
+						<h6>19/06/2021</h6>
+					</div>
+					<div class="col">
+						<b>Sun</b>
+						<h6>20/06/2021</h6>
+					</div>
+		<img src="https://img.favpng.com/6/23/17/computer-icons-png-favpng-Jj3idVigvuN67Lb48pw23xMZd.jpg" style="width: 45px;position: absolute;top:30px;left:-20px;cursor: pointer;">
+		<img src="https://e7.pngegg.com/pngimages/390/296/png-clipart-arrow-computer-icons-right-angle-symbol-arrow-left-angle-logo.png"style="width:45px;position: absolute;top:30px;right:-20px;cursor: pointer;">
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class="div-schedule">
+					<div class="schedule-title" style="padding: 0px;">
+					<p style="border-bottom: 2px solid; line-height: 35px; text-transform: uppercase; padding-top:10px">Morning</p>	
+					</div>	
+
+				<div class="schedule-title" style="padding: 0px;">
+					<p style="border-bottom: 2px solid;line-height: 35px;text-transform: uppercase;padding-top:10px">Afternoon</p>
+				</div>			
+				</div>
+
+</body>
+</html>
